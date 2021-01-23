aab1d1c4a5378bf97bd647cabd9ae0aa2e95afc3
Kristin (Kikki) Beltz <kdb05506@gmail.com>
2021-01-23 10:22:03 -0600

    Update project name and references

--- angular-frontend/karma.conf.js
+++ angular-frontend/karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/todo-frontend'),
+      dir: require('path').join(__dirname, './coverage/angular-frontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
