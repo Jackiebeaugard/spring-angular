ba277b74d4fa394cfe77c0fe3fdc9d674ae30b10
Matt Payne <Payne@MattPayne.org>
2021-02-02 17:14:03 -0600

    Use command line runner interface to put a task in the database.

--- spring-backend/src/main/java/com/example/task_manager/TaskManagerApplication.java
+++ spring-backend/src/main/java/com/example/task_manager/TaskManagerApplication.java
@@ -1,13 +1,23 @@
 package com.example.todo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodoApplication {
+public class TaskManagerApplication implements CommandLineRunner {
+
+  @Autowired
+  com.example.todo.repository.TaskRepository taskRepository;
 
   public static void main(String[] args) {
-    SpringApplication.run(TodoApplication.class, args);
+    SpringApplication.run(TaskManagerApplication.class, args);
   }
 
+  @Override
+  public void run(String... args) throws Exception {
+    com.example.todo.model.Task walkPoppy = new com.example.todo.model.Task("Walk Poppy");
+    taskRepository.save(walkPoppy);
+  }
 }
