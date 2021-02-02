d01a6ad5674c3669533135a57739c46b10b743f4
Matt Payne <Payne@MattPayne.org>
2021-02-02 17:14:30 -0600

    Line up API with the proxy.conf.json angular is using.

--- spring-backend/src/main/java/com/example/task_manager/controller/TaskController.java
+++ spring-backend/src/main/java/com/example/task_manager/controller/TaskController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @CrossOrigin
 @RestController
-@RequestMapping(value = "/todo")
+@RequestMapping(value = "/api/todo")
 public class TaskController {
   @Autowired
   private TaskRepository taskRepository;
